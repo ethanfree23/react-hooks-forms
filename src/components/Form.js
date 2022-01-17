be6668ec21e15559625ed242c1e779d5d6d8a13d
Ethan Freeman <ethanfree23@gmail.com>
2022-01-17 14:08:01 -0600

    Completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,13 +4,24 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+  }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+  }
+
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input onChange={handleFirstNameChange} type="text" value={firstName} />
+      <input onChange={handleLastNameChange} type="text" value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
 }
 
+
+
 export default Form;
+// in order to change a state, you need to create a destructed array, with the first value being the current state, and the second value being a function that sets the change.
